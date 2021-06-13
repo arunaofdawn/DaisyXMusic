f0fa2521e63b1110b0a67aaeed46c643e6fb7c63
arunaofdawn <85207407+arunaofdawn@users.noreply.github.com>
2021-06-13 11:24:53 +0700

    Update ytsearch.py

--- DaisyXMusic/modules/ytsearch.py
+++ DaisyXMusic/modules/ytsearch.py
@@ -44,8 +44,8 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
+            text += f"Judul - {results[i]['title']}\n"
+            text += f"Durasi - {results[i]['duration']}\n"
             text += f"Views - {results[i]['views']}\n"
             text += f"Channel - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
